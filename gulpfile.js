6721da738ed2bb4f2315cb605f85b6a57bd8abc7
keepcalmpolly <k.calm-polly@yandex.ru>
2022-12-02 15:29:25 +0300

    проект (2 модуль)

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,8 @@ gulp.task('clean', function () {
 gulp.task('copy', function() {
   return gulp.src([
       paths.src + '/img/**/*',
-      paths.src + '/fonts/**/*'
+      paths.src + '/fonts/**/*',
+      paths.src + '/audio/**/*'
     ], {
       base: paths.src
     })
