fb9bc3946f1cf2251b03e74b415cf2842fd6df13
keepcalmpolly <k.calm-polly@yandex.ru>
2022-12-07 14:12:14 +0300

    валидация 80%, адаптив стр гридов, ломаный бургер и многое другое
    2 модуль- done

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,6 +1,6 @@
-const popup = document.querySelector('.popup') 
-const popupBtn = document.querySelector('.button-js') 
-const popupClose = document.querySelector('.close-js') 
+// const popup = document.querySelector('.popup') 
+// const popupBtn = document.querySelector('.button-js') 
+// const popupClose = document.querySelector('.close-js') 
 
 // const mobileMenu = document.querySelector('.header__mobile') 
 // const mobileOpenBtn = document.querySelector('.open-js') 
@@ -16,10 +16,41 @@ const popupClose = document.querySelector('.close-js')
 
 
  
-popupBtn.addEventListener('click', function() { 
-    popup.classList.add('open') 
-}) 
+// popupBtn.addEventListener('click', function() { 
+//     popup.classList.add('open') 
+// }) 
  
-popupClose.addEventListener('click', function() { 
-    popup.classList.remove('open') 
-})
+// popupClose.addEventListener('click', function() { 
+//     popup.classList.remove('open') 
+// })
+
+(function(){ 
+    const mobileMenu = document.querySelector('.header__mobile')  
+    console.log(mobileMenu) 
+    const mobileOpenBtn = document.querySelector('.header__hamburger')  
+    console.log(mobileOpenBtn) 
+    const mobileCloseBtn = document.querySelector('.close-js')  
+    console.log(mobileCloseBtn) 
+     
+    mobileOpenBtn.addEventListener('click', function() {  
+        mobileMenu.classList.add('mobile__open')  
+    })  
+      
+    mobileCloseBtn.addEventListener('click', function() {  
+        mobileMenu.classList.remove('mobile__open')  
+    }) 
+})(); 
+ 
+(function() { 
+    const popup = document.querySelector('.popup')  
+    const popupBtn = document.querySelector('.button-js')  
+    const popupClose = document.querySelector('.close-js')  
+      
+    popupBtn.addEventListener('click', function() {  
+        popup.classList.add('open')  
+    })  
+      
+    popupClose.addEventListener('click', function() {  
+        popup.classList.remove('open')  
+    }) 
+})()
